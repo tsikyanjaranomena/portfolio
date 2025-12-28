ac9f8876e20b251d774fbc50aab45f31602c415d
tsikyanjaranomena <tsikyanjaranomena@gmail.com>
2025-12-28 21:36:04 +0300

    [fix] deployement

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
